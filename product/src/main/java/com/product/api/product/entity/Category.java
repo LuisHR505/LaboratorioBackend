bdb59ac0fb0b0870231943e0e5bb7023846d640b
LuisHR505 <luis.hernandez.505@ciencias.unam.mx>
2025-03-19 21:21:15 -0600

    Implementacion del doc C7 con respecto a category

--- product/src/main/java/com/product/api/product/entity/Category.java
+++ product/src/main/java/com/product/api/product/entity/Category.java
@@ -1,63 +1,84 @@
 package com.product.api.product.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Category")
 public class Category {
+	// agregamos los decoradores necesarios
 	@Id
-    private Integer category_id = 0;
-    private String category = "";
-    private String tag ="";
-    private Integer status = 0;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonProperty("category_id")
+	@Column(name = "category_id")
+	private Integer category_id = 0;
+	
+	@JsonProperty("category")
+	@Column(name = "category")
+	private String category = "";
+	
+	@JsonProperty("tag")
+	@Column(name = "tag")
+	private String tag = "";
+	
+	@JsonProperty("status")
+	@Column(name = "status")
+	private Integer status = 0;
 
-    public Category(Integer category_id, String category, String tag, Integer status){
-            this.category_id=category_id;
-            this.category=category;
-            this.tag=tag;
-            this.status=status;
-    }
-    
-    public Category() {
-        // Este constructor es necesario para JPA
-    }
+	public Category(Integer category_id, String category, String tag, Integer status) {
+		this.category_id = category_id;
+		this.category = category;
+		this.tag = tag;
+		this.status = status;
+	}
 
-    //metodos get
-    public int getId(){
-            return this.category_id;
-    }
-    public String getCategoryName(){
-            return this.category;
-    }
-    public String getTag(){
-            return this.tag;
-    }
-    public int getStatus(){
-            return this.status;
-    }
-    //metodos set
-    public void setStatus(Integer status){
-            this.status=status;
-    }
-    
-    public void setCategoryId(Integer category_id) {
-            this.category_id = category_id;
-    }
+	public Category() {
+		// Este constructor es necesario para JPA
+	}
 
-    public void setCategory(String category) {
-            this.category = category;
-    }
+	// metodos get
+	public int getId() {
+		return this.category_id;
+	}
 
-    public void setTag(String tag) {
-            this.tag = tag;
-    }
-    
+	public String getCategoryName() {
+		return this.category;
+	}
 
-    @Override
-    public String toString(){
-            return "{"+category_id.toString()+","+category+","+tag+","+status+"}";
-    } 
+	public String getTag() {
+		return this.tag;
+	}
+
+	public int getStatus() {
+		return this.status;
+	}
+
+	// metodos set
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public void setCategoryId(Integer category_id) {
+		this.category_id = category_id;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	@Override
+	public String toString() {
+		return "{" + category_id.toString() + "," + category + "," + tag + "," + status + "}";
+	}
 
 }
--- product/src/main/java/com/product/api/product/service/SvcCategory.java
+++ product/src/main/java/com/product/api/product/service/SvcCategory.java
@@ -4,9 +4,19 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 
+import com.product.api.commons.dto.ApiResponse;
+import com.product.api.product.dto.DtoCategoryIn;
 import com.product.api.product.entity.Category;
 
 public interface SvcCategory {
-	
+
 	public ResponseEntity<List<Category>> getCategories();
+	
+	// Practica 5: actualizamos la interfaz del servicio Category
+	public ResponseEntity<List<Category>> getActiveRegions();
+	public ResponseEntity<ApiResponse> createRegion(DtoCategoryIn in);
+	public ResponseEntity<ApiResponse> updateRegion(DtoCategoryIn in, Integer id);
+	public ResponseEntity<ApiResponse> enableRegion(Integer id);
+	public ResponseEntity<ApiResponse> disableRegion(Integer id);
+
 }
